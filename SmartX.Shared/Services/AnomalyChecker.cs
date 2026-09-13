bf2444a6d96710cd1a9fcb2db005d86b01b224d0
ForbesCaleb <St10438433@imconnect.edu.za>
2026-09-13 16:26:47 +0200

    Added AnomalyChecker

---
+++ SmartX.Shared/Services/AnomalyChecker.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartX.Shared.Models;
+
+public static class AnomalyChecker
+{
+    public static bool IsOutOfRange<T>(T value, T min, T max) where T : IComparable<T>
+        => value.CompareTo(min) < 0 || value.CompareTo(max) > 0;
+}
--- SmartX.Shared/obj/Debug/net10.0/SmartX.Shared.AssemblyInfo.cs
+++ SmartX.Shared/obj/Debug/net10.0/SmartX.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartX.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f33b159a37882dd90dbf654a9ba06457ee5257")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ef058044450473163dcb05de7e9e61001ab121")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartX.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartX.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
