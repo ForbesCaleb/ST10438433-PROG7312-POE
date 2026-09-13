3e90d81414b0724dbdc6ca355d67779bb5d7474e
ForbesCaleb <St10438433@imconnect.edu.za>
2026-09-13 16:02:04 +0200

    Added DeploymentNode model
    This is for the nested deployment

---
+++ SmartX.Shared/Models/DeploymentNode.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartX.Shared.Models;
+
+public class DeploymentNode
+{
+    public string Name { get; set; } = string.Empty;
+    public bool IsConfigured { get; set; }
+    public List<DeploymentNode> Children { get; set; } = new();
+}
--- SmartX.Shared/obj/Debug/net10.0/SmartX.Shared.AssemblyInfo.cs
+++ SmartX.Shared/obj/Debug/net10.0/SmartX.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartX.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50674d2d04fb784839247f4614284379ac213353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a34c3cfc1300dbd3732addb9d915e73dff1bad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartX.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartX.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
